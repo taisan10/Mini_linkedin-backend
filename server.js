f3839b580a714937c74feb1483e56e9cf90c6326
taisan10 <tarunbansalsgnr@gmail.com>
2025-08-25 15:09:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routers/authRoutes');
+const postRoutes = require('./routers/postRoutes');
+const cors = require('cors');
+const profileRoutes = require('./routers/profileRoutes');
+
+dotenv.config();
+const app = express();
+
+// const allowedOrigins = ['http://localhost:5000', 'http://localhost:5173'];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     }
+//     callback(new Error('Not allowed by CORS'));
+//   },
+//   credentials: true,
+// }));
+
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.json()); // To parse JSON bodies
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
+// Use the auth routes
+app.use('/api', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/profile/', profileRoutes);
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
